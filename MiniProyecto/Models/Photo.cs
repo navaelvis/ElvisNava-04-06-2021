186b3253c87bb986e5d10af0f42501d3f9ad693e
Elvis Nava <navaelvis@gmail.com>
2021-06-04 17:07:45 -0400

    Add project files.

---
+++ MiniProyecto/Models/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniProyecto.Models
+{
+    public class Photo
+    {
+        public int albumId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+    }
+}
